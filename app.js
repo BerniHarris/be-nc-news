26daa11354a8c81e2468fd7c725348878dd8ae0e
BerniHarris <harrisbernigmail.com>
2022-02-16 11:22:45 +0000

    amend layout of app.js to make it easier to read

--- app.js
+++ app.js
@@ -5,15 +5,13 @@ const app = express();
 
 app.get('/api/topics', getTopics)
 
-module.exports = app;
-
-app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).send('Server Error!');
-  });
-
 app.all('/*', (req, res) => {
   res.status(404).send({ message: 'path not found'});
 });
 
-// where does listen go? - not needed for tests - further on
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send('Server Error!');
+});
+
+module.exports = app;
