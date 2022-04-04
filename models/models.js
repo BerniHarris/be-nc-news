d1299d01c30ddf7d845730b15f337379892900b1
BerniHarris <harrisberni@gmail.com>
2022-04-04 17:01:16 +0100

    create fetch articles model

--- models/models.js
+++ models/models.js
@@ -1,59 +1,73 @@
-const db = require('../db/connection'); 
-const topics = require('../db/data/test-data/topics');
+const db = require("../db/connection");
+const topics = require("../db/data/test-data/topics");
 
 const fetchTopics = () => {
-    return db
-    .query("SELECT * FROM topics;")
-    .then(({rows}) => { 
-        return {topics: rows}
-    })
-}
+  return db.query("SELECT * FROM topics;").then(({ rows }) => {
+    return { topics: rows };
+  });
+};
 
 const fetchArticleById = (article_id) => {
-    return db
+  return db
     .query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
-        .then(({ rows }) => {
-         if(!rows[0]) { // custom error
-             return Promise.reject({ status: 404, msg: 'Article id not found. Please check and try again :)' })
-         }
-         return rows[0]; // ---- future berni this is the article object :)
-        })
-    } 
-
-const updateArticle = (article_id, inc_votes) => {
-    if(!inc_votes) {
+    .then(({ rows }) => {
+      if (!rows[0]) {
+        // custom error
         return Promise.reject({
-            status: 400,
-            msg: "Please include missing fields"
+          status: 404,
+          msg: "Article id not found. Please check and try again :)",
         });
-    } else
-        return db
-          .query(
-            `UPDATE articles 
-            SET votes = (votes + $1) 
-            WHERE article_id = $2 RETURNING *;`,
-            [inc_votes, article_id])
-          .then(({ rows }) => {
-            if(!rows[0]) { // custom error
-                return Promise.reject({ 
-                    status: 404,
-                    msg: 'Article id not found. Please check and try again :)' })
-            }
-            return rows[0];
-          });
-      };
-
-const fetchUserNames = () => {
-    return db
-    .query("SELECT username FROM users;")
-    .then(({rows}) => {
-        return rows
+      }
+      return rows[0]; // ---- future berni this is the article object :)
     });
 };
 
+const updateArticle = (article_id, inc_votes) => {
+  if (!inc_votes) {
+    return Promise.reject({
+      status: 400,
+      msg: "Please include missing fields",
+    });
+  } else
+    return db
+      .query(
+        `UPDATE articles 
+            SET votes = (votes + $1) 
+            WHERE article_id = $2 RETURNING *;`,
+        [inc_votes, article_id]
+      )
+      .then(({ rows }) => {
+        if (!rows[0]) {
+          // custom error
+          return Promise.reject({
+            status: 404,
+            msg: "Article id not found. Please check and try again :)",
+          });
+        }
+        return rows[0];
+      });
+};
 
-// Model: Handles the fetching, updating, creating and deleting of data, 
-// and sends the data in the required format to the controller based on controller’s 
+const fetchUserNames = () => {
+  return db.query("SELECT username FROM users;").then(({ rows }) => {
+    return rows;
+  });
+};
+
+const fetchArticles = () => {
+  return db.query("SELECT * FROM articles;").then(({ rows }) => {
+    return { articles: rows };
+  });
+};
+
+// Model: Handles the fetching, updating, creating and deleting of data,
+// and sends the data in the required format to the controller based on controller’s
 // instructions.
 
-module.exports = {fetchTopics, fetchArticleById, updateArticle, fetchUserNames} 
+module.exports = {
+  fetchTopics,
+  fetchArticleById,
+  updateArticle,
+  fetchUserNames,
+  fetchArticles,
+};
