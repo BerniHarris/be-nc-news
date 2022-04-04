950fba2772878928fe0028ae172a9843a66b2222
BerniHarris <harrisberni@gmail.com>
2022-04-04 16:59:08 +0100

    import and add GET articles

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
-const express = require('express');
-const { getTopics } = require('./controllers/topicControllers')
-const { getArticleById, patchArticle } = require('./controllers/articleControllers')
-const { getUserNames } = require('./controllers/userControllers')
-const {error404, customError, psqlError, error500} = require('./errors/errors')
+const express = require("express");
+const { getTopics } = require("./controllers/topicControllers");
+const {
+  getArticleById,
+  patchArticle,
+  getArticles,
+} = require("./controllers/articleControllers");
+const { getUserNames } = require("./controllers/userControllers");
+const {
+  error404,
+  customError,
+  psqlError,
+  error500,
+} = require("./errors/errors");
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/api/topics', getTopics);
-app.get('/api/articles/:article_id', getArticleById);
-app.get('/api/users', getUserNames);
+app.get("/api/topics", getTopics);
+app.get("/api/articles/:article_id", getArticleById);
+app.get("/api/users", getUserNames);
+app.get("/api/articles", getArticles);
 
-app.patch('/api/articles/:article_id', patchArticle)
+app.patch("/api/articles/:article_id", patchArticle);
 
-app.all('/*', error404)
+app.all("/*", error404);
 
-app.use(customError)
-app.use(psqlError)
-app.use(error500)
-
+app.use(customError);
+app.use(psqlError);
+app.use(error500);
 
 module.exports = app;
