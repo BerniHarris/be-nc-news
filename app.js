8053b5f3265b8a4049e671690977bcf00ffa1f89
BerniHarris <harrisberni@gmail.com>
2022-04-04 17:06:05 +0100

    add header comments to make easier to read

--- app.js
+++ app.js
@@ -14,19 +14,19 @@ const {
 } = require("./errors/errors");
 const app = express();
 
+//-------USE------
 app.use(express.json());
-
+app.use(customError);
+app.use(psqlError);
+app.use(error500);
+//-------GET------
 app.get("/api/topics", getTopics);
 app.get("/api/articles/:article_id", getArticleById);
 app.get("/api/users", getUserNames);
 app.get("/api/articles", getArticles);
-
+//-------PATCH------
 app.patch("/api/articles/:article_id", patchArticle);
-
+//-------ALL------
 app.all("/*", error404);
-
-app.use(customError);
-app.use(psqlError);
-app.use(error500);
 
 module.exports = app;
