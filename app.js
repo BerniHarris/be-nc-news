f167abe0514b8d328f8006a2e306357cd261ebd2
BerniHarris <harrisberni@gmail.com>
2022-04-05 23:50:25 +0100

    organise to read easier. add get article comments

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const {
   getArticleById,
   patchArticle,
   getArticles,
+  getArticleComments,
 } = require("./controllers/articleControllers");
 const { getUserNames } = require("./controllers/userControllers");
 const {
@@ -14,19 +15,24 @@ const {
 } = require("./errors/errors");
 const app = express();
 
-//-------USE------
 app.use(express.json());
+
+//-------GET------
+app.get("/api/topics", getTopics);
+app.get("/api/users", getUserNames);
+app.get("/api/articles", getArticles);
+app.get("/api/articles/:article_id", getArticleById);
+app.get("/api/articles/:article_id/comments", getArticleComments);
+
+//-------PATCH------
+app.patch("/api/articles/:article_id", patchArticle);
+
+//-------ALL------
+app.all("/*", error404);
+
+//-------USE------
 app.use(customError);
 app.use(psqlError);
 app.use(error500);
-//-------GET------
-app.get("/api/topics", getTopics);
-app.get("/api/articles/:article_id", getArticleById);
-app.get("/api/users", getUserNames);
-app.get("/api/articles", getArticles);
-//-------PATCH------
-app.patch("/api/articles/:article_id", patchArticle);
-//-------ALL------
-app.all("/*", error404);
 
 module.exports = app;
