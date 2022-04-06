ea9eed14ac3e441b84396d69f5aeab12628b7f0d
BerniHarris <harrisberni@gmail.com>
2022-04-07 00:15:38 +0100

    update error message

--- errors/errors.js
+++ errors/errors.js
@@ -1,30 +1,42 @@
-const error404 = ('/*', (req, res) => {
-    res.status(404).send({ message: 'Path not found.'});
+const error404 =
+  ("/*",
+  (req, res) => {
+    res.status(404).send({ message: "Path not found." });
   });
-  
-  // ---- custom errors ----
-  const customError = (err, req, res, next) => {
-    if( err.status && err.msg) {
-      res.status(err.status).send({ message: err.msg })
-    } else {
-      next(err) // don't forget this moves you down to next app block
-    }
-  }
-  
-  // ---- PSQL errors ----
-  const psqlError = (err, req, res, next) => {
-    if (err.code === "22P02") {
-      res.status(400).send({ message: 'Not a valid article id. Please check your id number and try again'})
-    }  else {
-      next(err)
-    }
-  }
-  
-  // ---- final error! ----
-  const error500 = (err, req, res, next) => {
-    console.log(err);
-    res.status(500).send('Server Error!');
-  };
-  
 
-  module.exports = {error404,customError,psqlError,error500}
+// ---- custom errors ----
+const customError = (err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ message: err.msg });
+  } else {
+    next(err); // don't forget this moves you down to next app block
+  }
+};
+
+// ---- PSQL errors ----
+const psqlError = (err, req, res, next) => {
+  if (err.code === "22P02") {
+    res
+      .status(400)
+      .send({
+        message:
+          "Not a valid article id. Please check your id number and try again",
+      });
+  } else if (err.code === "23502") {
+    res
+      .status(400)
+      .send({
+        message: `Don't forget to include your username and comment body!`,
+      });
+  } else {
+    next(err);
+  }
+};
+
+// ---- final error! ----
+const error500 = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).send("Server Error!");
+};
+
+module.exports = { error404, customError, psqlError, error500 };
