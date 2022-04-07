b3c0eba49be8debe817e0407594f9c61c0d42a60
BerniHarris <harrisberni@gmail.com>
2022-04-07 01:06:15 +0100

    add custom errors

--- errors/errors.js
+++ errors/errors.js
@@ -16,18 +16,18 @@ const customError = (err, req, res, next) => {
 // ---- PSQL errors ----
 const psqlError = (err, req, res, next) => {
   if (err.code === "22P02") {
-    res
-      .status(400)
-      .send({
-        message:
-          "Not a valid article id. Please check your id number and try again",
-      });
+    res.status(400).send({
+      message:
+        "Not a valid article id. Please check your id number and try again",
+    });
   } else if (err.code === "23502") {
-    res
-      .status(400)
-      .send({
-        message: `Don't forget to include your username and comment body!`,
-      });
+    res.status(400).send({
+      message: `Don't forget to include your username and comment body!`,
+    });
+  } else if (err.code === "23503") {
+    res.status(404).send({
+      message: `Input not found. Please try again`,
+    });
   } else {
     next(err);
   }
